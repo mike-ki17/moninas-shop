4cb56a2dd63dfb0bf94964bbc36088af657c7eea
mike-ki17 <michael16cd@gmail.com>
2025-05-31 21:05:07 -0500

    chore: reajuste carpetas

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/moninas-shop/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/moninas-shop/'
 });
